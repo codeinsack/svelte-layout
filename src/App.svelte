d48299580ed4faf83da2d61ea8e6fe65e9b61c39
codeinsack <codeinsack@gmail.com>
2021-12-07 18:26:53 +0300

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<main>
+  <h1>Hello {name}! My age is {age}</h1>
+  <button class="btn btn-success" type="button" on:click={incrementAge}>
+    Increment age
+  </button>
+</main>
+
+<script>
+  let name = 'Eugene'
+  let age = 32
+
+  const incrementAge = () => {
+    age++
+  }
+</script>
+
+<style>
+</style>
